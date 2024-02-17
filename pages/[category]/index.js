97dac23c8589216bd7a2b94a685fe7c29ef7905b
Yashu9084 <yashumittal9084@gmail.com>
2024-02-17 08:43:40 +0530

    new search bar added

--- pages/[category]/index.js
+++ pages/[category]/index.js
@@ -18,143 +18,147 @@ import iscity from "@/lib/request/iscity/isCity";
 import VendorCategoryCardGrid from "@/components/miscellaneous/vendorcategorycardgrid/VendorCategoryCardGrid";
 import { query } from "@/utils/db";
 
-export default function Home({ venueCategogies, cities, popularVenues, blogposts, vendorCategories,city }) {
+export default function Home({
+  venueCategogies,
+  cities,
+  popularVenues,
+  blogposts,
+  vendorCategories,
+  vendor_list,
+  venue_list,
+  city,
+}) {
+  const { setSelectedCity } = useGlobalContext();
 
-    const {setSelectedCity} = useGlobalContext();
-    
-    useEffect(()=>{
-        if(city){
-            setSelectedCity(city);
-        }
-    },[city])
+  useEffect(() => {
+    if (city) {
+      setSelectedCity(city);
+    }
+  }, [city]);
 
-    // City name is comming from the dynamic router (params). We are setting in the global context
-    return (
-        <div>
-
-            <Navbar />
-            <Hero venueCategogies={venueCategogies} cities={cities} />
-            <VendorSlider vendorCategories={vendorCategories} />
-            <PopularVenue popularVenues={popularVenues} />
-            <HowItWorks />
-            <LeadForm/>
-            <Blog posts={blogposts} />
-            <WhyUs />
-            <Contact />
-            <VendorCategoryCardGrid/>
-            <CityVenueHall cities={cities}/>
-            
-        </div>
-    )
+  // City name is comming from the dynamic router (params). We are setting in the global context
+  return (
+    <div>
+      <Navbar />
+      <Hero
+        venueCategogies={venueCategogies}
+        vendorCategories={vendorCategories}
+        vendor_list={vendor_list}
+        venue_list={venue_list}
+        cities={cities}
+      />
+      <VendorSlider vendorCategories={vendorCategories} />
+      <PopularVenue popularVenues={popularVenues} />
+      <HowItWorks />
+      <LeadForm />
+      <Blog posts={blogposts} />
+      <WhyUs />
+      <Contact />
+      <VendorCategoryCardGrid />
+      <CityVenueHall cities={cities} />
+    </div>
+  );
 }
 
+export async function getServerSideProps({ params, req, res }) {
+  const { category: slug } = params;
 
-export async function getServerSideProps({params,req,res}) {
+  //Checking if the url is city or not, iscity is the method which takes city slug and return boolean value accordingly.
+  if (await iscity(slug)) {
+    // console.log("City is " + slug)
+    // console.log( await iscity(slug))
+    try {
+      //Fetch Blog Post
+      // let homePageData = await fetch("http://192.168.29.128/wedding_benquets/website/api/home_page/");
 
-    const { category: slug } = params;
+      const url = `${process.env.SERVER_DOMAIN}/api/home_page/`;
 
-    //Checking if the url is city or not, iscity is the method which takes city slug and return boolean value accordingly.
-    if ( await iscity(slug)) {
-        // console.log("City is " + slug)
-        // console.log( await iscity(slug))
-        try {
+      // const headers = { 'Authorization': process.env.SECRATE_TOKEN };
+      // console.log(url)
 
+      let homePageData = await fetch(url);
 
-            //Setting the response for browser cache, This is for optimization only. 
-            // res.setHeader(
-            //     'Cache-Control',
-            //     'public, s-maxage=900, stale-while-revalidate=1200'
-            //   )
+      homePageData = await homePageData.json();
+      // console.log(homePageData)
 
+      const url2 = `${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/api/search_form_result_vendor`;
+      let vendor_list = await fetch(url2);
+      vendor_list = await vendor_list.json();
 
+      const url3 = `${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/api/search_form_result_venue`;
+      let venue_list = await fetch(url3);
+      venue_list = await venue_list.json();
 
-            const url = `${process.env.SERVER_DOMAIN}/api/home_page/${slug}`
+      const url4 = `${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/api/state_management`;
+      let context_data = await fetch(url4);
+      context_data = await context_data.json();
 
-
-            // let homePageData = await fetch(`http://192.168.29.128/wedding_benquets/website/api/home_page/${slug}`);
-
-            let homePageData = await fetch(url)
-            // console.log(city)
-          
-            homePageData = await homePageData.json();
-            // console.log(homePageData)
-
-
-            let blogposts;
-            try {
-              const sql = `select wp_posts.ID, wp_posts.post_title, wp_posts.post_name as
+      let blogposts;
+      try {
+        const sql = `select wp_posts.ID, wp_posts.post_title, wp_posts.post_name as
               post_slug, DATE_FORMAT(wp_posts.post_date, '%Y-%m-%d %T.%f') as post_date, (select for_image.guid from wp_posts as
               for_image where for_image.post_parent = wp_posts.ID and for_image.post_type =
               "attachment" limit 1) as post_thumbnail from wp_posts where
               (wp_posts.post_type = 'post' and wp_posts.post_status = 'publish') order by
               wp_posts.ID desc limit 3`;
-        
-        
-              blogposts = await query(sql);
-            //   console.log("Blog data fetched")
-        
-            } catch (error) {
-            //   console.log("Blog data not fetched")
-              blogposts = [];
-            }
 
-            return {
-                props: {
-                    venueCategogies: homePageData.data.venue_categories || null,
-                    vendorCategories: homePageData.data.vendor_categories || null,
-                    cities: homePageData.data.cities || null,
-                    popularVenues: homePageData.data.popular_venues || null,
-                    blogposts: blogposts || null,
-                    city: slug || null          //To set the city on the global context
-                }
-            }
+        blogposts = await query(sql);
+        console.log("Blog data fetched");
+      } catch (error) {
+        console.log("Blog data not fetched");
+        blogposts = [];
+      }
 
-        } catch (error) {
-            console.log(error)
-            return {
-                notFound: true
-            }
-
-        }
+      return {
+        props: {
+          venueCategogies: homePageData.data.venue_categories || null,
+          vendorCategories: homePageData.data.vendor_categories || null,
+          vendor_list: vendor_list || null,
+          venue_list: venue_list || null,
+          cities: homePageData.data.cities || null,
+          popularVenues: homePageData.data.popular_venues || null,
+          blogposts: blogposts || null,
+        },
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        notFound: true,
+      };
     }
-    else {      //This is magix box which redirect the old url to the new url with its coressponding page.
+  } else {
+    //This is magix box which redirect the old url to the new url with its coressponding page.
 
+    let url = slug?.split("-in-");
 
-        let url = slug?.split("-in-");              
-     
-        //It means its a listing page url 
-        if (url.length === 2) {
+    //It means its a listing page url
+    if (url.length === 2) {
+      if (url[1] === "delhi") {
+        url[1] = "all";
+      }
 
+      return {
+        redirect: {
+          permanent: true, //This is permanent reirection 301. setting this permanent means telling the google that we have moved our old url to new url permanet and our old url is no more valid.
+          destination: `/${url[0]}/delhi/${url[1]}`,
+        },
+        props: {
+          status: 1,
+        },
+      };
+    } else if (url.length === 1) {
+      //It means its a details page url
 
-            if (url[1] === 'delhi') {
-                url[1] = "all";
-            }
-
-            return {
-                redirect: {
-                    permanent: true,        //This is permanent reirection 301. setting this permanent means telling the google that we have moved our old url to new url permanet and our old url is no more valid.
-                    destination: `/${url[0]}/delhi/${url[1]}`,
-                },
-                props: {
-                    status: 1
-                },
-            };
-        }
-        else if (url.length === 1) {                //It means its a details page url 
-
-            return {
-                redirect: {
-                    permanent: true,
-                    destination: `/delhi/${url[0]}`,
-                },
-            };
-        }
-        else {
-            return {
-                notFound
-            }
-        }
+      return {
+        redirect: {
+          permanent: true,
+          destination: `/delhi/${url[0]}`,
+        },
+      };
+    } else {
+      return {
+        notFound,
+      };
     }
-
+  }
 }
-
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-
 import Blog from "@/components/homepage/blog/Blog";
 import Contact from "@/components/homepage/contactUs/Contact";
 import Hero from "@/components/homepage/hero/Hero";
@@ -20,57 +19,60 @@ import { query } from "@/utils/db";
 // const HowItWorks = dynamic(import("../components/homepage/howitwork/HowItWorks"))
 // import Testimonial from "@/components/homepage/testimonial/Testimonial";
 
-
-
-
-export default function Home({ venueCategogies, cities, popularVenues, blogposts, vendorCategories }) {
-
+export default function Home({
+  venueCategogies,
+  cities,
+  popularVenues,
+  blogposts,
+  vendorCategories,
+  vendor_list,
+  venue_list
+}) {
   const jsonLdData = {
     "@context": "https://schema.org",
     "@type": "ItemList",
-    "itemListElement": [
+    itemListElement: [
       {
         "@type": "ListItem",
-        "position": 1,
-        "url": "https://weddingbanquets.in/banquet-halls/delhi/all",
-        "name": "Banquet Halls"
+        position: 1,
+        url: "https://weddingbanquets.in/banquet-halls/delhi/all",
+        name: "Banquet Halls",
       },
       {
         "@type": "ListItem",
-        "position": 2,
-        "url": "https://weddingbanquets.in/party-halls/delhi/all",
-        "name": "Party Halls"
+        position: 2,
+        url: "https://weddingbanquets.in/party-halls/delhi/all",
+        name: "Party Halls",
       },
       {
         "@type": "ListItem",
-        "position": 3,
-        "url": "https://weddingbanquets.in/marriage-gardens/delhi/all",
-        "name": "Marriage Gardens"
+        position: 3,
+        url: "https://weddingbanquets.in/marriage-gardens/delhi/all",
+        name: "Marriage Gardens",
       },
       {
         "@type": "ListItem",
-        "position": 4,
-        "url": "https://weddingbanquets.in/top-makeup-artists/delhi/all",
-        "name": "Makeup Artists"
+        position: 4,
+        url: "https://weddingbanquets.in/top-makeup-artists/delhi/all",
+        name: "Makeup Artists",
       },
       {
         "@type": "ListItem",
-        "position": 5,
-        "url": "https://weddingbanquets.in/best-wedding-photographers/delhi/all",
-        "name": "Photographers"
+        position: 5,
+        url: "https://weddingbanquets.in/best-wedding-photographers/delhi/all",
+        name: "Photographers",
       },
       {
         "@type": "ListItem",
-        "position": 6,
-        "url": "https://weddingbanquets.in/best-mehndi-artists/delhi/all",
-        "name": "Mehndi Artists"
-      }
-    ]
+        position: 6,
+        url: "https://weddingbanquets.in/best-mehndi-artists/delhi/all",
+        name: "Mehndi Artists",
+      },
+    ],
   };
 
   return (
     <div>
-
       <Head>
         <script
           type="application/ld+json"
@@ -78,8 +80,14 @@ export default function Home({ venueCategogies, cities, popularVenues, blogposts
         />
       </Head>
 
-      <Navbar />
-      <Hero venueCategogies={venueCategogies} cities={cities} />
+      <Navbar/>
+      <Hero
+        venueCategogies={venueCategogies}
+        vendorCategories={vendorCategories}
+        vendor_list={vendor_list}
+        venue_list={venue_list}
+        cities={cities}
+      />
       <VendorSlider vendorCategories={vendorCategories} />
       <PopularVenue popularVenues={popularVenues} />
       <HowItWorks />
@@ -93,27 +101,35 @@ export default function Home({ venueCategogies, cities, popularVenues, blogposts
       {/* <FooterVendors/> */}
       <CityVenueHall cities={cities} />
     </div>
-  )
+  );
 }
 
-
 export async function getStaticProps() {
-
   try {
-
     //Fetch Blog Post
     // let homePageData = await fetch("http://192.168.29.128/wedding_benquets/website/api/home_page/");
 
-    const url = `${process.env.SERVER_DOMAIN}/api/home_page/`
-
+    const url = `${process.env.SERVER_DOMAIN}/api/home_page/`;
 
     // const headers = { 'Authorization': process.env.SECRATE_TOKEN };
     // console.log(url)
 
-    let homePageData = await fetch(url)
+    let homePageData = await fetch(url);
 
     homePageData = await homePageData.json();
-    // console.log(homePageData.data.blogs)
+    // console.log(homePageData)
+
+    const url2 = `${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/api/search_form_result_vendor`;
+    let vendor_list = await fetch(url2);
+    vendor_list = await vendor_list.json();
+
+    const url3 = `${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/api/search_form_result_venue`;
+    let venue_list = await fetch(url3);
+    venue_list = await venue_list.json();
+
+    const url4 = `${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/api/state_management`;
+    let context_data = await fetch(url4);
+    context_data = await context_data.json();
 
     let blogposts;
     try {
@@ -124,12 +140,10 @@ export async function getStaticProps() {
       (wp_posts.post_type = 'post' and wp_posts.post_status = 'publish') order by
       wp_posts.ID desc limit 3`;
 
-
       blogposts = await query(sql);
-      console.log("Blog data fetched")
-
+      console.log("Blog data fetched");
     } catch (error) {
-      console.log("Blog data not fetched")
+      console.log("Blog data not fetched");
       blogposts = [];
     }
 
@@ -137,19 +151,17 @@ export async function getStaticProps() {
       props: {
         venueCategogies: homePageData.data.venue_categories || null,
         vendorCategories: homePageData.data.vendor_categories || null,
+        vendor_list: vendor_list || null,
+        venue_list: venue_list || null,
         cities: homePageData.data.cities || null,
         popularVenues: homePageData.data.popular_venues || null,
-        blogposts: blogposts || null
-      }
-    }
-
-
+        blogposts: blogposts || null,
+      },
+    };
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return {
-      notFound: true
-    }
-
+      notFound: true,
+    };
   }
-
 }
